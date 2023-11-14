b9b16c098edad47606cecc57993dcfe011ddc1c4
Shyam Prakash J <kirthikajayaraman1594@gmail.com>
2023-11-14 20:05:48 +0530

    es6-2

---
+++ DOM-3/Hospital_Dr_Records/index.js
@@ -0,0 +1,38 @@
+let form = document.querySelector('form');
+let tbody = document.querySelector('tbody');
+
+form.addEventListener('submit', submitData);
+
+function submitData(event) {
+    event.preventDefault();
+    let name = form.name.value;
+    let doctorID = form.docID.value;
+    let specialization = form.dept.value;
+    let exp = form.exp.value;
+    let email = form.email.value;
+    let mbl = form.mbl.value;
+
+    let role = exp<=1 ? 'Fresher': exp>=2 && exp<=5 ? 'Junior': exp>5 ? 'Senior': null;
+
+    createRow(name, doctorID, specialization, exp, email, mbl, role, 'Delete');
+}
+
+function createRow(name, empId, dep, exp, email, mob, role, del) {
+    let paramArr = [name, empId, dep, exp, email, mob, role, del];
+    let tr = document.createElement('tr');
+    
+    for(let param of paramArr) {
+        let td = document.createElement('td');
+        td.innerText = param;
+        if(param == del) {
+                td.addEventListener('click', (event)=> {
+                event.target.parentNode.remove();
+            })
+            td.style.cursor = 'default'
+            td.style.backgroundColor = 'red'
+            td.style.color = 'black'
+        }
+        tr.append(td);
+    }
+    tbody.append(tr);
+}
